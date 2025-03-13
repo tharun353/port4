5a7f6ea1fc910a879036addb667e9f75180e4d96
tharun353 <tharunnarasimman76@gmail.com>
2025-03-13 23:34:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// // Dark Mode Toggle
+// const darkModeToggle = document.getElementById('dark-mode-toggle');
+// darkModeToggle.addEventListener('click', () => {
+//     document.body.classList.toggle('dark-mode');
+//     darkModeToggle.textContent = document.body.classList.contains('dark-mode') ? '☀' : '🌙';
+// });
